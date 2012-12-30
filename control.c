e2f9920ec43aa12b584117651b8392645e2a8b8f
Gabriel Candal <gabrielcandal@gmail.com>
2012-12-30 15:31:24 +0000

    Reorganizacao codigo, faltou adicionar

---
+++ control.c
@@ -0,0 +1,124 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include "control.h"
+
+int kbd_subscribe_int(void ) {
+
+	sys_irqsetpolicy(KBD_IRQ,IRQ_REENABLE|IRQ_EXCLUSIVE,&kbdhook);
+	sys_irqenable(&kbdhook);
+
+	return 1;
+}
+
+int kbd_unsubscribe_int() {
+
+	sys_irqrmpolicy(&kbdhook);
+	sys_irqdisable(&kbdhook);
+
+	return 1;
+}
+
+static unsigned char kbd_readscancode() {
+
+	 unsigned long data, stat;
+
+	 while( 1 ) {
+		 sys_inb(STAT_REG, &stat); /* assuming it returns OK */
+
+		 /* loop while 8042 output buffer is empty */
+		 if( stat & OBF ) {
+			 sys_inb(OUT_BUF, &data); /* assuming it returns OK */
+
+			 if ( (stat &(PAR_ERR | TO_ERR)) == 0 )
+				 return (unsigned char)data;
+			 else return -1;
+		 }
+
+		 tickdelay(micros_to_ticks(DELAY_US));
+	 }
+}
+
+int kbd_int_handler(const unsigned int shots) {
+
+	unsigned char scancode; //Will store C@KBD responses
+
+	scancode = kbd_readscancode();
+
+	if(scancode==-1) {
+		printf("Reading Error from Status Register");
+		return -1;
+	}
+
+	if(scancode==SPACE_BREAK && shots>0)
+		return 1;
+
+	if(scancode==ESC_BREAK)
+		return 0;
+
+	return -1;
+}
+
+int mouse_subscribe_int() {
+
+	sys_irqsetpolicy(MOUSE_IRQ,IRQ_REENABLE|IRQ_EXCLUSIVE,&mousehook);
+	sys_irqenable(&mousehook);
+
+	return 1;
+}
+
+int mouse_unsubscribe_int() {
+
+	sys_irqrmpolicy(&mousehook);
+	sys_irqdisable(&mousehook);
+
+	return 1;
+}
+
+int mouse_int_handler(int* x, int* y, int* posx_inicial, int* posy_inicial, int H_RES, int V_RES) {
+
+	sys_inb(IN_BUF,&stat);
+
+	int confirma=0, changed=0;
+
+	if( !(stat&BIT(3)) && (count==0))
+		confirma=1;
+
+	packet[count]=stat;
+	count++;
+
+	if(count==3 && confirma==0) {
+		count=0;
+
+		if(packet[1]!=0 || packet[2]!=0) {
+			changed=1;
+
+			if(packet[1]!=0) {
+				if(packet[1]<128)
+					*posx_inicial+=packet[1];
+				else *posx_inicial+=packet[1]-256;
+
+				if(*posx_inicial<0)
+					*posx_inicial=0;
+				else if(*posx_inicial>(H_RES-40))
+					*posx_inicial=H_RES-40;
+
+				*x=*posx_inicial;
+			}
+
+			if(packet[2]!=0) {
+				if(packet[2]<128)
+					*posy_inicial-=packet[2];
+				else *posy_inicial-=packet[2]-256;
+
+				if(*posy_inicial<0)
+					*posy_inicial=0;
+				else if(*posy_inicial>(V_RES-100))
+					*posy_inicial=V_RES-100;
+
+				*y=*posy_inicial;
+			}
+		}
+	}
+
+	return changed;
+}
---
+++ control.h
@@ -0,0 +1,94 @@
+#ifndef _CONTROL_H_
+#define _CONTROL_H_
+
+/** @defgroup control control
+ * @{
+ *
+ * Functions for using and interpreting keyboard/mouse responses in order to move the spaceship
+ */
+
+#define STAT_REG 			0x64
+#define OUT_BUF				0x60
+#define IN_BUF 				0x60 //or 0x64
+#define KBD_IRQ				1
+#define MOUSE_IRQ			12
+#define DELAY_US 			20000
+
+//C@KBD responses
+#define ESC_MAKE			0x01
+#define ESC_BREAK			0x81
+#define SPACE_BREAK			0x39
+#define SPACE_MAKE			0xB9
+
+#define BIT(n) 				(0x01<<(n))
+
+//Status Register
+#define OBF					BIT(0) // =1: full; Outbuffer
+#define PAR_ERR				BIT(7)
+#define TO_ERR				BIT(6)
+
+//Mouse commands
+#define ENABLE_MOUSE		0xA8
+#define ENABLE_DATA			0xF4
+#define WRITE_BYTE			0xD4 //commands the KBC to forward its argument to the mouse
+
+static int mousehook=3, kbdhook=5;
+static unsigned char packet[3];
+static unsigned short count=0;
+static unsigned long stat;
+
+/**
+ * @brief Subscribes and enables keyboard interrupts
+ *
+ * @return Return 0 upon success
+ */
+int kbd_subscribe_int();
+/**
+ * @brief Unsubscribes keyboard interrupts
+ *
+ * @return Return 0 upon success
+ */
+int kbd_unsubscribe_int();
+/**
+ * @brief Reads the scancode from the C@KBD
+ *
+ * @return Returns the scancode
+ */
+static unsigned char kbd_readscancode();
+/**
+ * @brief Interprets keyboard interrupt
+ *
+ * Reads information from keyboard and interprets it.
+ *
+ * @param shots Number of shots the spaceship can still do
+ * @return Returns -1 if no relevant response, 1 if a shot has been fired and 0 if ESC was pressed.
+ */
+int kbd_int_handler(const unsigned int shots);
+/**
+ * @brief Subscribes and enables mouse interrupts
+ *
+ * @return Return 0 upon success
+ */
+int mouse_subscribe_int();
+/**
+ * @brief Unsubscribes mouse interrupts
+ *
+ * @return Return 0 upon success
+ */
+int mouse_unsubscribe_int();
+/**
+ * @brief Interprets mouse interrupt
+ *
+ * Reads information from the mouse and interprets it.
+ *
+ * @param x Ship's coordinates
+ * @param y Ship's coordinates
+ * @param posx_inicial Ship's coordinates
+ * @param posy_inicial Ship's coordinates
+ * @param H_RES Horizontal screen resolution
+ * @param V_RES Vertical screen resolution
+ * @return Returns -1 if no relevant response, 1 if a shot has been fired and 0 if ESC was pressed.
+ */
+int mouse_int_handler(int* x, int* y, int* posx_inicial, int* posy_inicial, int H_RES, int V_RES);
+
+#endif
